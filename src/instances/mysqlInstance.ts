bfb9350cad22ba8ac5f474ecd3babf15abc41bf0
Kelvem Loubach <kelvemloubach@outlook.com>
2023-04-04 23:02:47 -0300

    Deploy Elephant 2

--- src/instances/mysqlInstance.ts
+++ src/instances/mysqlInstance.ts
@@ -4,15 +4,16 @@ dotenv.config()
 
 export  const sequelizeMysqlConection = new Sequelize(
 
-    process.env.DB_URL as string
+    process.env.DB_URL as string,
 
-    
+
 //      process.env.MYSQL_DB as string,
 //      process.env.MYSQL_USER as string,
 //      process.env.MYSQL_PASSWORD as string,
-//     {
-//         dialect:'mysql',
-//         port: parseInt(process.env.MYSQL_PORT as string)
-//     } ,  
+
+    {
+        dialect:'postgres',
+        port: parseInt(process.env.MYSQL_PORT as string)
+    }   
 );
 
