b7619a7625e38654b433a38ed22c98c52b20ed14
Kelvem Loubach <kelvemloubach@outlook.com>
2023-04-04 21:50:16 -0300

    Deploy mysql

--- src/instances/mysqlInstance.ts
+++ src/instances/mysqlInstance.ts
@@ -7,7 +7,7 @@ export  const sequelizeMysqlConection = new Sequelize(
      process.env.MYSQL_USER as string,
      process.env.MYSQL_PASSWORD as string,
     {
-        dialect:'postgres',
+        dialect:'mysql',
         port: parseInt(process.env.MYSQL_PORT as string)
     } ,  
 );
